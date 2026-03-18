8c51715892d230f4a5cddb92cf1b8f1179cf61ab
Klipar <bhdnkarpenko123@gmail.com>
2026-03-18 23:27:27 +0100

    add get all function template

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,31 @@ pub struct Question {
 pub fn get_greeting() -> String {
     String::from("Hello from the quizzer library!")
 }
+
+pub fn get_question() -> Vec<Question> {
+    let mut questions = Vec::new();
+    questions.push(Question {
+        question: "2 + 2 = ?".to_string(),
+        options: [
+            "3".to_string(),
+            "4".to_string(),
+            "5".to_string(),
+            "6".to_string(),
+        ],
+    });
+
+    questions.push(Question {
+        question: "Столиця Франції?".to_string(),
+        options: [
+            "Лондон".to_string(),
+            "Париж".to_string(),
+            "Берлін".to_string(),
+            "Мадрид".to_string(),
+        ],
+    });
+    questions.push(Question {
+        question: "Otakza od zaka fresko?".to_string(),
+        options: ["Tak".to_string(), "niiii".to_string(), "may be".to_string(), "xto ja".to_string()]
+    });
+    questions
+}
